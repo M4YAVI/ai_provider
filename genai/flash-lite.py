e0c98a6560859173a0ea2abcdc470d87b6c09f20
Aryayama <85403809+M4YAVI@users.noreply.github.com>
2025-07-11 09:14:52 +0530

    Update flash-lite.py

--- genai/flash-lite.py
+++ genai/flash-lite.py
@@ -3,19 +3,17 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-# Load environment variables from .env file
 load_dotenv()
 
 # The Client will read GEMINI_API_KEY from the environment
 client = genai.Client()
 
-# Use the streaming method
 stream = client.models.generate_content_stream(
     model="gemini-2.5-flash-lite-preview-06-17",
     contents="Summarize the latest advancements in AI.",
 )
 
-# Print the streamed response as it arrives
+ 
 for chunk in stream:
     print(chunk.text, end="", flush=True)
-print()  # for a final newline
+print()  
